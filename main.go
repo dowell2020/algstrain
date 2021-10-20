a5203341f7257937872fbd61c8dd93f706e8f002
dowell <dowell@macPro.local>
2021-10-20 17:37:04 +0800

    update 排序

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/dowell2020/algstrain/algs13"
+)
 
 func main() {
-	fmt.Println(".......")
+
+	// // FindSubstring
+	// s := "wordgoodgoodgoodbestword"
+	// words := []string{"word", "good", "best", "word"}
+
+	// res := algs12.FindSubstring(s, words)
+	// nums := []int{-1, 2, 1, -4}
+	// target := 1
+	// res := algs13.ThreeSumClosest(nums, target)
+
+	nums := []int{1, 0, -1, 0, -2, 2}
+	target := 0
+	res := algs13.FourSum(nums, target)
+	fmt.Println(res)
+	// fmt.Println(".......")
 }
